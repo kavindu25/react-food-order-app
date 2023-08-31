9e8812a8c8fa04774668c005e3022b2530cd5f65
kavindu25 <kavindu25@github.com>
2023-08-31 23:47:56 +0530

    fix: fixed meal item form styling issue

--- webapp/src/components/ShopItems/MealItem/MealItem.js
+++ webapp/src/components/ShopItems/MealItem/MealItem.js
@@ -5,7 +5,7 @@ const MealItem = (props) => {
   const formattedPrice = `$${props.price.toFixed(2)}`;
 
   return (
-    <li classname={classes.meal}>
+    <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
