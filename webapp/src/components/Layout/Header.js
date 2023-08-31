edfb677851ff2b8f9d7d919ec4120e2745fb541d
kavindu25 <kavindu25@github.com>
2023-08-31 20:21:41 +0530

    feat: created header component

---
+++ webapp/src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <button>Order</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of food" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
